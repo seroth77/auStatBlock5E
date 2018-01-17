837c131bf01c8cb4f3b4dc1725ca91a8b1fe138a
sero <seroth77@gmail.com>
2018-01-17 15:12:58 -0500

    Added and linked some components

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 export class App {
-  constructor() {
+  constructor(statBlock) {
+    this.statBlock = statBlock;
     this.message = 'Hello World!';
+    this.itemArray = [];
+  }
+
+  newItem() {
+    let obj = {};
+    this.itemArray.push(obj);
   }
 }
